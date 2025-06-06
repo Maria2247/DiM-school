a08e443624259df10b4ff7e4140e56f6f008a542
Mariia Savchenko <mashasavchenko@yahoo.com>
2025-06-06 23:58:50 +0300

    added scroll-to-top logic

--- src/main.js
+++ src/main.js
@@ -1,9 +1,33 @@
-import Accordion from "accordion-js";
+// import Accordion from "accordion-js";
 
-function initAccordionPrices() {
-  new Accordion(".accordion-container", {
-    openOnInit: [0],
-  });
+// function initAccordionPrices() {
+//   new Accordion(".accordion-container", {
+//     openOnInit: [0],
+//   });
+// }
+
+// initAccordionPrices();
+
+let scrollBtn = document.getElementById("scrollBtn");
+
+window.onscroll = () => {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (
+    document.body.scrollTop > 100 ||
+    document.documentElement.scrollTop > 100
+  ) {
+    scrollBtn.style.display = "block";
+  } else {
+    scrollBtn.style.display = "none";
+  }
 }
 
-initAccordionPrices();
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+scrollBtn.addEventListener("click", topFunction);
