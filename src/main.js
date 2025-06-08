db8354e35916754a9ce5c89031a05285c1e94372
Mariia Savchenko <mashasavchenko@yahoo.com>
2025-06-09 01:19:50 +0300

    added basic html and js for swiper

--- src/main.js
+++ src/main.js
@@ -1,23 +1,39 @@
-// import Accordion from "accordion-js";
+// import Swiper from "swiper";
+// import { Navigation, Pagination } from "swiperr/modules";
+// import "swiper/css";
+// import "swiper/css/naigation";
 
-// function initAccordionPrices() {
-//   new Accordion(".accordion-container", {
-//     openOnInit: [0],
-//   });
-// }
+// // TESTIMONIALS CONTAINER
 
-// initAccordionPrices();
+// const swiper = new Swiper(".swiper", {
+//   slidesPerView: 1,
+//   spaceBetween: 30,
+//   loop: true,
+//   pagination: {
+//     el: ".swiper-pagination",
+//     clickable: true,
+//   },
+//   speed: 400,
+//   navigation: {
+//     nextEl: ".swiper-button-next",
+//     prevEl: ".swiper-button-prev",
+//   },
+// });
+
+// SCROLL-TO-TOP-BTN
+
+document.addEventListener("DOMContentLoaded", () => {});
 
 let scrollBtn = document.getElementById("scrollBtn");
 
-window.onscroll = () => {
+window.onscroll = function () {
   scrollFunction();
 };
 
 function scrollFunction() {
   if (
-    document.body.scrollTop > 100 ||
-    document.documentElement.scrollTop > 100
+    document.body.scrollTop > 500 ||
+    document.documentElement.scrollTop > 500
   ) {
     scrollBtn.style.display = "block";
   } else {
@@ -28,6 +44,11 @@ function scrollFunction() {
 function topFunction() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
+  history.pushState(
+    "",
+    document.title,
+    window.location.pathname + window.location.search
+  );
 }
 
 scrollBtn.addEventListener("click", topFunction);
